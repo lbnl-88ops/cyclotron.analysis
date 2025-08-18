90a865040a1345c129a1203f4eb39d521daeb36f
Jessica Rehak <rehak@lbl.gov>
2025-08-18 10:15:12 -0700

    added trim coil current limits and solver

---
+++ ops/cyclotron/analysis/trim_coils/current_limits.py
@@ -0,0 +1,18 @@
+from typing import List
+
+from ops.cyclotron.analysis.model import TrimCoil
+
+def _get_default_limits(coil_number: int) -> tuple:
+    match coil_number:
+        case 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 10 | 11:
+            return (None, 750)
+        case 12 | 13 | 14 | 15: 
+            return (None, 2000)
+        case 16 | 17:
+            return (None, 2500)
+        case _:
+            return (0, 0)
+
+def update_current_limits(trim_coils: List[TrimCoil]) -> None:
+    for coil in trim_coils:
+            coil.set_current_limits(_get_default_limits(coil.number))
